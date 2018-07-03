00c209947a365a2f9bd6daa913592ccc62a05550
christianmader <christian.mader@univie.ac.at>
2018-07-03 18:28:56 +0200

    add basic jackson serialization/deserialization test

--- src/test/java/de/fraunhofer/iais/eis/ids/TestSerialization.java
+++ src/test/java/de/fraunhofer/iais/eis/ids/TestSerialization.java
@@ -1,23 +1,51 @@
 package de.fraunhofer.iais.eis.ids;
 
-import de.fraunhofer.iais.eis.BrokerDataRequest;
-import de.fraunhofer.iais.eis.BrokerDataRequestAction;
-import de.fraunhofer.iais.eis.BrokerDataRequestBuilder;
-import de.fraunhofer.iais.eis.EntityCoveredByDataRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.fraunhofer.iais.eis.*;
 import de.fraunhofer.iais.eis.util.ConstraintViolationException;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 public class TestSerialization {
 
     private BrokerDataRequest request;
+    private DataTransfer dataTransfer;
 
     @Before
-    private void setUp() throws ConstraintViolationException {
+    public void setUp() throws ConstraintViolationException {
         request = new BrokerDataRequestBuilder()
                 .dataRequestAction(BrokerDataRequestAction.REGISTER)
                 .coveredEntity(EntityCoveredByDataRequest.CONNECTOR)
                 .messageContent("Hello world")
                 .build();
+
+        TransferAttribute transferAttribute = new TransferAttributeBuilder()
+                .transferAttributeKey("key")
+                .transferAttributeValue("value")
+                .build();
+
+        dataTransfer = new DataTransferBuilder()
+                .authToken(new AuthTokenBuilder().tokenValue("dummyToken").build())
+                .customAttributes(Arrays.asList(transferAttribute))
+                .build();
+    }
+
+    @Test
+    public void deserializeBrokerDataRequest() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        String brokerDataRequest = mapper.writeValueAsString(this.request);
+        String dataTransfer = mapper.writeValueAsString(this.dataTransfer);
+
+        DataTransfer deserializedTransfer = mapper.readValue(dataTransfer, DataTransferImpl.class);
+        BrokerDataRequestImpl deserializedDataRequest = mapper.readValue(brokerDataRequest, BrokerDataRequestImpl.class);
+
+        Assert.assertNotNull(deserializedTransfer);
+        Assert.assertNotNull(deserializedDataRequest);
     }
 
 }
