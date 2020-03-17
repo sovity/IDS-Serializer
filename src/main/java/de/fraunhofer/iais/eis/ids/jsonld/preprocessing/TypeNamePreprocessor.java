2f5869921941ad4058233e6eeee9f02375c16341
sbader <sebastian.bader@iais.fraunhofer.de>
2020-03-17 13:47:33 +0100

    adding Catalog with an JSON-LD Array and testing it

--- src/main/java/de/fraunhofer/iais/eis/ids/jsonld/preprocessing/TypeNamePreprocessor.java
+++ src/main/java/de/fraunhofer/iais/eis/ids/jsonld/preprocessing/TypeNamePreprocessor.java
@@ -3,7 +3,9 @@ package de.fraunhofer.iais.eis.ids.jsonld.preprocessing;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
@@ -33,6 +35,8 @@ public class TypeNamePreprocessor extends BasePreprocessor {
 		Map<Object, Object> out = new LinkedHashMap<>();
 		in.forEach((k,v) -> {
 			if(v instanceof String && k instanceof String && k.equals("@type")) {
+
+
 				// if key is a reserved term and value is a string: add 'ids:' if no other namespace at the value
 				AtomicReference<String> modifiableValue = new AtomicReference<>((String) v);
 				prefixes.forEach((p, u) -> modifiableValue.set(modifiableValue.get().replace(u, p))); // replace full URI with prefix
@@ -43,7 +47,11 @@ public class TypeNamePreprocessor extends BasePreprocessor {
 					modifiableValue.set("ids:".concat(modifiableValue.get())); // default to ids prefix for backwards compatibility
 				}
 				out.put(k, modifiableValue.get());
+
+
 			} else if(v instanceof Map) {
+
+
 				AtomicReference<String> modifiableKey = new AtomicReference<>((String) k);
 				prefixes.forEach((p, u) -> modifiableKey.set(modifiableKey.get().replace(u, p))); // replace full URI with prefix
 				if(! (modifiableKey.get().startsWith("ids:")
@@ -51,9 +59,30 @@ public class TypeNamePreprocessor extends BasePreprocessor {
 						|| modifiableKey.get().startsWith("kdsf:"))) {
 					modifiableKey.set("ids:".concat(modifiableKey.get())); // default to ids prefix for backwards compatibility
 				}
+
+
 				out.put(modifiableKey, unifyTypeURIPrefix((Map) v));
-			}
-			else {                
+
+
+//			} else if(v instanceof ArrayList) {
+//
+//
+//				AtomicReference<String> modifiableKey = new AtomicReference<>((String) k);
+//				prefixes.forEach((p, u) -> modifiableKey.set(modifiableKey.get().replace(u, p))); // replace full URI with prefix
+//				if(! (modifiableKey.get().startsWith("ids:")
+//						|| modifiableKey.get().startsWith("info:")
+//						|| modifiableKey.get().startsWith("kdsf:"))) {
+//					modifiableKey.set("ids:".concat(modifiableKey.get())); // default to ids prefix for backwards compatibility
+//				}				
+//
+//				Iterator iter = ((ArrayList) v).iterator();
+//				while (iter.hasNext()) {
+//					out.put(modifiableKey, unifyTypeURIPrefix((Map) iter.next())); // TODO: What happens with an Array inside the Array?
+//				}
+
+			} else {
+
+
 				AtomicReference<String> modifiableKey = new AtomicReference<>((String) k);
 				prefixes.forEach((p, u) -> modifiableKey.set(modifiableKey.get().replace(u, p))); // replace full URI with prefix
 				if(! (modifiableKey.get().startsWith("ids:")
